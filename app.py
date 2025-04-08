86d73e57074b22846adaddd1e57322319f9f51c6
ambika <ambikavundakode@gmail.com>
2025-04-08 10:19:31 +0530

    added file app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import boto3
+client = boto3.client("ec2", region_name="us-east-1")
+response = client.run_instances(
+    ImageId='ami-071226ecf16aa7d96',
+    InstanceType='t2.micro',
+    KeyName= 'pythontest',
+    MaxCount=1,
+    MinCount=1
+)
